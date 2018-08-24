9295570600604461dd1d2940eaead2f143577c48
Renan Rodrigo <renanrb@ime.usp.br>
2018-08-24 09:16:09 -0300

    Adicionar funcionalidade de movimento na barra lateral

---
+++ script.js
@@ -0,0 +1,11 @@
+let botao = document.querySelector("#hamburger");
+let lateral = document.querySelector("aside");
+
+botao.onclick = function(){
+    lateral.classList.toggle('mostrando');
+    for(item of lateral.children){
+        if(item.id !== "hamburger"){
+            item.classList.toggle("invisivel");
+        }
+    }
+}
